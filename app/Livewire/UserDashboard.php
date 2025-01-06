db88b1d4fb73a953b144c1ecb3a77c996de3e571
deknaa <kdkrisnaaa@gmail.com>
2025-01-06 16:23:36 +0800

    feat: added todo and displayed it

--- app/Livewire/UserDashboard.php
+++ app/Livewire/UserDashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Todolist;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
@@ -10,12 +11,27 @@ class UserDashboard extends Component
     public function render()
     {
         $users = Auth::user();
-        return view('livewire.user-dashboard', compact('users'));
+        $todos = Todolist::where('user_id', $users->id)->get();
+        return view('livewire.user-dashboard', compact('users', 'todos'));
     }
 
     public function logout()
     {
         Auth::logout();
         return redirect()->route('login');
+    }
+
+    public $todo;
+
+    public function addTask()
+    {
+        $this->validate([
+            'todo' => 'required|min:3|string',
+        ]);
+
+        Todolist::create([
+            'todo' => $this->todo,
+            'user_id' => Auth::user()->id
+        ]);
     }
 }
